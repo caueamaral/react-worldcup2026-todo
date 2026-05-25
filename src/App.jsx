ab637c10c71ff8e00f8d70775c371140992c8bc9
Cauê Amaral <caue.amaral@hotmail.com>
2026-05-25 10:17:48 -0300

    style: limit app container width

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container mx-auto px-5">
+    <div className="container mx-auto max-w-xl px-5">
       <Header />
       <Main />
     </div>
